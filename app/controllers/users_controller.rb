d71ec0c8d78ba06f21a112a272f2e4b50f0dc6b1
Kevin <ktawaststjerna@gmail.com>
2016-11-06 21:35:13 -0500

    set up controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,64 +1,28 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  # GET /users
-  # GET /users.json
   def index
-    @users = User.all
+    render json: {}, status: :not_implementedl
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
+    render json: {}, status: :not_implemented
   end
 
-  # GET /users/new
   def new
-    @user = User.new
+    render json: {}, status: :not_implemented
   end
 
-  # GET /users/1/edit
-  def edit
-  end
-
-  # POST /users
-  # POST /users.json
   def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+    render json: {}, status: :not_implemented
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+    render json: {}, status: :not_implemented
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    render json: {}, status: :not_implemented
   end
 
   private
