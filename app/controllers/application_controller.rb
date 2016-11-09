5656354fa1d35263911cef6f0c2f0e0b5554fc64
Kevin <ktawaststjerna@gmail.com>
2016-11-09 00:03:31 -0500

    Trying to add logout stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
+  # layout "main"
   protect_from_forgery with: :exception
-  def main
-    render json: {}, status: :ok
-  end
-  
+  include SessionsHelper
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,16 @@ class SessionsController < ApplicationController
     return render json: {error: "Username not found"}, status: :unauthorized unless user = User.find_by(name: params[:session][:name])
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
+      log_in user
+      redirect_to user
     else
       # Create an error message.
       render json: {Error: "Incorrect password silly!"}, status: :unauthorized
     end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
   end
 end
