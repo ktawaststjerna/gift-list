9aa71f34ffb0602a096fab6dd8b6a616c684102f
Kevin <ktawaststjerna@gmail.com>
2016-11-10 22:10:26 -0500

    goes to item index

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,9 @@ class SessionsController < ApplicationController
     return render json: {error: "Username not found"}, status: :unauthorized unless user = User.find_by(name: params[:session][:name])
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
+      item = Item.where(user_id: user.id)
       log_in user
-      redirect_to user
+      redirect_to controller: 'items', action: 'index'
     else
       # Create an error message.
       render json: {Error: "Incorrect password silly!"}, status: :unauthorized
