7d354a001eaf44b7a1f35ceef0d53440bd50f3df
Kevin <ktawaststjerna@gmail.com>
2016-11-22 23:11:49 -0500

    pipes created and items showing on views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161106230423) do
+ActiveRecord::Schema.define(version: 20161123230424) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "families", force: :cascade do |t|
     t.string   "name"
@@ -30,8 +33,19 @@ ActiveRecord::Schema.define(version: 20161106230423) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.index ["family_id"], name: "index_items_on_family_id"
-    t.index ["user_id"], name: "index_items_on_user_id"
+    t.index ["family_id"], name: "index_items_on_family_id", using: :btree
+    t.index ["user_id"], name: "index_items_on_user_id", using: :btree
+  end
+
+  create_table "pipes", force: :cascade do |t|
+    t.integer  "from_id"
+    t.string   "from_type"
+    t.integer  "to_id"
+    t.string   "to_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["from_id", "from_type"], name: "index_pipes_on_from_id_and_from_type", using: :btree
+    t.index ["to_id", "to_type"], name: "index_pipes_on_to_id_and_to_type", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
