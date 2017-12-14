ca9ef6178d6b5906d4837c61ced1f3d39f30f2eb
Kevin <ktawaststjerna@gmail.com>
2017-12-13 21:46:24 -0500

    Method on user to add items to familes

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
   has_many :items
 
   validates :name, presence: true
+
+  #Great for new users who need items on new families
+  def add_items_to_families(family)
+    puts "Must be of class family" if family.class != Family
+    items.each do |item|
+      Pipe.create(from: family, to: item)
+    end
+  end
 end
