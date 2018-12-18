3dbf4492748724091aeb61f53ae975d76728dcc5
kevlarz <ktawaststjerna@gmail.com>
2018-12-17 19:21:03 -0500

    Remove quantity validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,6 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_many :families, :through => :to_pipes, :source => :from, :source_type => 'Family'
-  
-  validates :user, :name, :quantity, :link, presence: true
-  # validates :family, presence: true
+
+  validates :user, :name, :link, presence: true
 end
