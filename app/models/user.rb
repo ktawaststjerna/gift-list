01785dc0e22075d6a56af6c10a700551a1818081
Kevin Tawaststjerna <12237417+ktawaststjerna@users.noreply.github.com>
2021-08-24 13:26:35 -0700

    Refactor family items (#45)

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true
 
-  #Great for new users who need items on new families
-  def add_items_to_families(family)
-    puts "Must be of class family" if family.class != Family
-    items.each do |item|
-      Pipe.create(from: family, to: item)
-    end
+  def family_items
+    families.map(&:items).flatten.uniq
   end
 end
