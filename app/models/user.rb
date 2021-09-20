912d061d81f0fc510c9a8cc9fbef64f3db59eaf9
Kevin Tawaststjerna <ktawaststjerna@gmail.com>
2021-09-20 15:04:32 -0400

    Issue #51: Removed archived items from gift list index

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,8 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :families, :through => :to_pipes, :source => :from, :source_type => 'Family'
+  has_many :family_items, -> { where(archived: false).distinct }, through: :families, source: :items
   has_many :items
 
   validates :name, presence: true
-
-  def family_items
-    families.map(&:items).flatten.uniq
-  end
 end
