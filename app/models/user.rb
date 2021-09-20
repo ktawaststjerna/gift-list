9713dc456674325aaf3b4be88bb15e99e031b85f
Kevin Tawaststjerna <12237417+ktawaststjerna@users.noreply.github.com>
2021-09-20 15:23:07 -0400

    Issue #53: Order items in giftlist by user id (#54)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :families, :through => :to_pipes, :source => :from, :source_type => 'Family'
-  has_many :family_items, -> { where(archived: false).distinct }, through: :families, source: :items
+  has_many :family_items, -> { where(archived: false).distinct.order(:user_id) }, through: :families, source: :items
   has_many :items
 
   validates :name, presence: true
